cb12e416a567b2eeafbd4d029937eddad75c4c27
Vishal648-pixel <vishalmasule903@gmail.com>
2026-09-08 20:21:24 +0530

    Initial commit

---
+++ program.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int assignment_operator();
+int comparision_operator();
+
+int main() {//arithermatic operator
+    // int a =28;
+    // int b=38;
+    // cout<<"addition :"<<a+b<<"\n";
+    // cout<<"subtraction :"<<a-b<<"\n";
+    // cout<<"multiplication :"<<a*b<<"\n";
+    // cout<<"division :"<<a/b<<"\n";
+    comparision_operator();
+    // assignment_operator();
+    return 0;
+}
+
+// int assignment_operator() {
+//     int x=38;
+//     int y=43;
+//     x+=y;
+//     cout<<x<<"x=x+y";
+//     return 0;
+// }
+
+
+// int comparision_operator() {
+//     int f=24;
+//     int g=93;
+//     cout<<(g>f);//if g is greater than f then it will print 1 otherwise 0
+//
+//
+//
+//     int age=5;
+//     cout<<(age>=18);
+//     return 0;
+// }
