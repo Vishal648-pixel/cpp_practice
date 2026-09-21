4eb875130065e1c0c62c4c339e0de54880adca72
Vishal masule <vishalmasule903@gmail.com>
2026-09-21 15:57:56 +0530

    Implement selection sort and print array functions
    Added selection sort and print array functions.

--- program.cpp
+++ program.cpp
@@ -1,7 +1,29 @@
-# include <iostream>
-# include <string>
+//
+// Created by mca on 21/09/26.
+//
+#include<iostream>
 using namespace std;
 
-int main(){
-    cout<<"First progrm";
+void selection_sort(int arr[],int size)
+{
+    for (int i=0;i<size-1;i++)
+    {
+        
+    }
+
+}
+
+void printarr(int arr[],int size)
+{
+    for (int i=0;i<=size-1;i++)
+    {
+        cout<<arr[i];
+    }
+}
+int main()
+{
+    int arr[]={64,87,99,35,24,76,12,0,84};
+    int size=sizeof(arr)/4;
+    cout<<"size :"<<size<<endl;
+
 }
