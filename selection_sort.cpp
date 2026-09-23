dc5b411613a19db3527a92afe526c6482df16d97
Vishal masule <vishalmasule903@gmail.com>
2026-09-23 15:34:30 +0530

    Add selection sort implementation in C++
    Implement selection sort algorithm and array printing.

---
+++ selection_sort.cpp
@@ -0,0 +1,34 @@
+//
+// Created by vishal on 22/09/26.
+//
+#include <iostream>
+using namespace std;\
+
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n){
+    for(int i = 0; i < n-1; i++){
+        int minIndex = i;
+        for(int j = i+1; j < n; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main() {
+    int arr[7]={10,4,65,32,0,2,-4};
+    int n=sizeof(arr)/4;
+    selectionSort(arr, n);
+    printArray(arr, n);
+}
